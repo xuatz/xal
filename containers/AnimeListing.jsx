eedf21a012267c9db077f2376d4508f47e138036
xuatz <xzlow10@gmail.com>
2016-05-04 00:51:12 +0800

    made the defaulting sorting list to show the last 30% aired anime first

--- containers/AnimeListing.jsx
+++ containers/AnimeListing.jsx
@@ -11,21 +11,30 @@ function pad(n, width, z) {
 
 
 const mapStateToProps = (state) => {
+	console.log('xz: value 1', state.animeListing[0]);
+
+
+	let series = state.animeListing.sort(function(a, b){
+		let valueA = '' + a.daysUntil + pad(a.hoursUntil, 2) + pad(a.minutesUntil, 2);
+		let valueB = '' + b.daysUntil + pad(b.hoursUntil, 2) + pad(b.minutesUntil, 2);
+
+		if (parseInt(valueA) > parseInt(valueB)) {
+			return 1;
+		}
+
+		if (parseInt(valueA) < parseInt(valueB)) {
+			return -1;
+		}
+
+		return 0;
+	});
+
+	let marker = Math.floor(0.7 * series.length);
+	let left = series.slice(0, marker);
+	let right = series.slice(marker);
+
 	return {
-		series: state.animeListing.sort(function(a, b){
-			let valueA = '' + a.daysUntil + pad(a.hoursUntil, 2) + pad(a.minutesUntil, 2);
-			let valueB = '' + b.daysUntil + pad(b.hoursUntil, 2) + pad(b.minutesUntil, 2);
-
-			if (parseInt(valueA) > parseInt(valueB)) {
-				return 1;
-			}
-
-			if (parseInt(valueA) < parseInt(valueB)) {
-				return -1;
-			}
-
-			return 0;
-		})
+		series: right.concat(left)
 	};
 }
 

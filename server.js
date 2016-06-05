07e64db5997e7259d1648ff515db8dedf882bb89
xuatz <xzlow10@gmail.com>
2016-06-06 01:29:26 +0800

    added login/create functionality in the header

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var webpack = require('webpack');
 var path = require('path');
 var http = require('http');
+var express = require('express');
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+
+// var Parse = require('parse/node');
+// var parseExpressHttpsRedirect = require('parse-express-https-redirect');
+// var parseExpressCookieSession = require('parse-express-cookie-session');
+// var PARSE_APP_ID = "Uclorbou8YGlgHjBg67zYockeUkH67KPovdjZ30D";
+// var JAVASCRIPT_KEY = "HQYLuwBlmnLs9QudSWcRgLZT9nAp47kluHvMVPNn";
+// var COOKIE_SECRET = 'placeholderCookieSecret';
+
 var PORT = process.env.PORT || 8080;
-var app = express();
+
 var httpServer = http.createServer(app);
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// //https://dashboard.back4app.com/apps#/wizard/app-details/d13c136a-4390-4b84-82f8-d611c1176417
+// Parse.initialize(PARSE_APP_ID, JAVASCRIPT_KEY);
+// Parse.serverURL = 'https://parseapi.back4app.com';
+// app.use(parseExpressHttpsRedirect());  // Require user to be on HTTPS.
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', function(req, res) {
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,136 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
 
-const Header = () => {
+import Parse from 'parse';
+
+const Logout = (props) => {
+	const handleOnClick = (event) => {
+		console.log('Logout:handleOnClick');
+		console.log(event);
+		Parse.User.logOut().then(() => {
+			props.updateLoginStatus();
+		});
+	}
 	return (
-		<nav>
-			{"*Placeholder Header* testing1 | testing2"}
-		</nav>
+		<div>
+			<button onClick={handleOnClick} >
+				Logout
+			</button>
+		</div>
 	);
+};
+
+const Login = (props) => {
+	const handleOnClick = (event) => {
+		console.log('Login:handleOnClick');
+	}
+
+	const handleOnSubmit = (event) => {
+		event.preventDefault();
+
+		console.log('Login:handeOnSubmit');
+		// console.log(event.target.username.value);
+		// console.log(event.target.password.value);
+
+		if (props.isLogin) {
+			Parse.User.logIn(event.target.username.value, event.target.password.value, {
+				success: function(user) {
+					// Do stuff after successful login.
+					console.log('login success!');
+					props.updateLoginStatus();
+
+				},
+				error: function(user, error) {
+					// The login failed. Check error to see why.
+					console.log('login failed!');
+				}
+			});
+		} else {
+			let user = new Parse.User();
+			user.set("username", event.target.username.value);
+			user.set("password", event.target.password.value);
+			user.signUp(null, {
+				success: function(user) {
+					// Hooray! Let them use the app now.
+					props.updateLoginStatus();
+				},
+				error: function(user, error) {
+					// Show the error message somewhere and let the user try again.
+					alert("Error: " + error.code + " " + error.message);
+				}
+			});	
+		}
+	}
+
+	return (
+		<form style={{float:'right'}} onSubmit={handleOnSubmit}>
+			<span style={{margin:'0px 10px', float:'left'}} onClick={props.handleOnClick} >
+				{props.isLogin ?
+					"Don't have an account? Create one now!" :
+					"Have an account? Log here!"
+				}
+			</span>
+			<span style={{float:'right'}}>
+				<input id="username" name="username" placeholder="username" type="text" />
+				<input id="password" name="password" placeholder="password" type="password" />
+				<button type="submit">
+					{props.isLogin ?
+						"Login" :
+						"Create"
+					}
+				</button>
+			</span>
+		</form>
+	);
+};
+
+class Header extends React.Component {
+	constructor(props) {
+		super();
+		this.state = {
+			isLogin: true,
+			action: '/placeholder',
+			loggedIn: Parse.User.current() ? true : false
+		}
+		this.handleOnClick = this.handleOnClick.bind(this);
+		this.updateLoginStatus = this.updateLoginStatus.bind(this);
+	}
+
+	handleOnClick(event) {
+		console.log('Header:handleOnClick');
+		console.log(event);
+		this.setState({
+			isLogin: !this.state.isLogin
+		})
+	}
+
+	updateLoginStatus() {
+		console.log('Header:updateLoginStatus');
+		console.log('loggedIn', this.state.loggedIn);
+
+		let loggedIn = Parse.User.current() ? true : false;
+		console.log(loggedIn);
+
+		this.setState({
+			loggedIn: loggedIn
+		});
+	}
+
+	render() {
+		return (
+			<div style={{display:'inline-block', width:'100%'}} >
+				<span>
+					{"*Placeholder Header* testing1 | testing2"}
+				</span>
+				<span style={{width:'40%', float:'right', margin:'20px'}} >
+					{this.state.loggedIn ? 
+						<Logout updateLoginStatus={this.updateLoginStatus} /> :
+						<Login isLogin={this.state.isLogin} handleOnClick={this.handleOnClick} updateLoginStatus={this.updateLoginStatus} />
+					}
+				</span>
+			</div>
+		);
+	}
 }
 
 export default Header;
